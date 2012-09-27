4f52f4e4bec95b6c179dda503a4f32a296a9f15d
tillage <i@tillage.net>
2012-09-27 08:48:25 +0800

    Update vagex_debian.sh

--- vagex_debian.sh
+++ vagex_debian.sh
@@ -8,6 +8,8 @@
 #  修改部分代码，并加入注释
 #备注：
 #	尽量在screen下运行以防中途断线
+#用法： 安装完成后用vnc连接后进行相应firefox优化后, File > Open File ，选择 Video Quality Manager 安装文件，安装后再安装 vagex_firefox_add_on*.xpi
+#       firefox优化无非就是关掉更新，关历史记录，更改首页为空白页面……
 
 ff_addr="http://download.cdn.mozilla.net/pub/mozilla.org/firefox/releases/3.6.26/linux-i686/en-US/firefox-3.6.26.tar.bz2"
 flash_addr="http://fpdownload.macromedia.com/get/flashplayer/pdc/11.1.102.55/install_flash_player_11_linux.i386.tar.gz"
